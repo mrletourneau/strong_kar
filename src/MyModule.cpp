85e921b3bd61998a802638f1721321d5a3d1c0b2
Alexander Letourneau <letourneau@samsix.com>
2019-06-14 20:43:52 -0400

    Segfaulting a bit, but got a hacky frequency knob in there

--- src/MyModule.cpp
+++ src/MyModule.cpp
@@ -4,7 +4,8 @@
 
 struct MyModule : Module {
     enum ParamId {
-        PITCH_PARAM,
+        DAMP_PARAM,
+        FREQ_PARAM,
         NUM_PARAMS
     };
     enum InputId {
@@ -16,8 +17,8 @@ struct MyModule : Module {
         NUM_OUTPUTS
     };
 
-    int samples = 1000;
-    float noise [1000];
+    int samplesPeriod = 0;
+    float* noise = NULL;
     int noisePointer = 0;
     float previousOutput = 0;
 
@@ -28,42 +29,62 @@ struct MyModule : Module {
 
         // Configure parameters
         // See engine/Param.hpp for config() arguments
-        configParam(PITCH_PARAM, -3.f, 3.f, 0.f, "Pitch", " Hz", 2.f, dsp::FREQ_C4);
+        configParam(DAMP_PARAM, .01f, .5f, 0.5f, "Damp", " Hz", 2.f, dsp::FREQ_C4);
+        configParam(FREQ_PARAM, -3.f, 3.f, 0.f, "Pitch", " Hz", 2.f, dsp::FREQ_C4);
 
-        regenerateTable();
+        noise = new float[1];
+        noise[0] = 0;
     }
 
-    void regenerateTable()
+    void regenerateTable(int frequency, float sampleRate)
     {
-        for (int i = 0; i < samples; i++)
+        delete [] noise;
+
+        samplesPeriod = sampleRate / frequency;
+
+        noise = new float[samplesPeriod];
+
+        for (int i = 0; i < samplesPeriod; i++)
         {
             noise[i] = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/5.0));
         }
     }
 
     void onReset() override {
-        regenerateTable();
+        delete [] noise;
+        noise = new float[1];
+        noise[0] = 0;
     }
 
     void process(const ProcessArgs &args) override {
-        float x = inputs[TRIGGER_INPUT].value;
+        float triggerVoltage = inputs[TRIGGER_INPUT].value;
 
         dsp::SchmittTrigger schmittTrigger;
-        schmittTrigger.process(rescale(x, 0.1f, 10.f, 0.f, 1.f));
+        schmittTrigger.process(rescale(triggerVoltage, 0.1f, 10.f, 0.f, 1.f));
         bool triggered = schmittTrigger.isHigh();
+
+        float pitch = params[FREQ_PARAM].getValue();
+        //pitch += inputs[PITCH_INPUT].getVoltage();
+        pitch = clamp(pitch, -4.f, 4.f);
+        // The default pitch is C4 = 261.6256f
+        float freq = dsp::FREQ_C4 * std::pow(2.f, pitch);
 
         if (triggered == 1)
         {
-            regenerateTable();
+            regenerateTable(freq, args.sampleRate);
         }
 
-        noise[noisePointer] = 0.5 * (noise[noisePointer] + previousOutput);
+
+        float dampAmount = params[DAMP_PARAM].getValue();
+
+
+        noise[noisePointer] = dampAmount * (noise[noisePointer] + previousOutput);
 
         outputs[PLUCK_OUTPUT].setVoltage(noise[noisePointer]);
 
         if (noisePointer == 0)
         {
-            previousOutput = noise[samples - 1];
+            previousOutput = noise[samplesPeriod - 1];
         }
         else
         {
@@ -72,7 +93,7 @@ struct MyModule : Module {
 
         noisePointer++;
 
-        if (noisePointer >= samples)
+        if (noisePointer >= samplesPeriod)
             noisePointer = 0;
     }
 };
@@ -88,7 +109,8 @@ struct MyModuleWidget : ModuleWidget {
         addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
         addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-        addParam(createParam<Davies1900hBlackKnob>(Vec(25, 87), module, MyModule::PITCH_PARAM));
+        addParam(createParam<Davies1900hBlackKnob>(Vec(25, 87), module, MyModule::DAMP_PARAM));
+        addParam(createParam<Davies1900hBlackKnob>(Vec(25, 130), module, MyModule::FREQ_PARAM));
 
         addInput(createInput<PJ301MPort>(Vec(25, 186), module, MyModule::TRIGGER_INPUT));
 
